bc9543521c7713ac8626c9ddf91c7a660f3eb24c
AnjumAK <113706527+AnjumAK@users.noreply.github.com>
2022-09-16 19:27:28 +0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+const Hapi = require('hapi');
+const server = Hapi.server({
+port :3000,
+host: 'localhost'
+});
+const mongoose = require(‘mongoose’);
+mongoose.connect(‘URL’, () => {
+   console.log(‘Connected to Mongo DB Successfully!!’);
+})
+const init = async () => {
+await server.start();
+server.route({
+methid : 'GET',
+path :'
+handler (‘/posts/comments’, (request, response) => {
+      const user = new User({
+         title : request.title,
+         body : request.body,
+         comments : request.comments,
+       
+      });
