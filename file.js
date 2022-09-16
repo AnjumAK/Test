ac33ccf643112907e75236d59d36c397770ebe63
AnjumAK <113706527+AnjumAK@users.noreply.github.com>
2022-09-16 20:13:08 +0400

    Update and rename user.js to file.js

---
+++ file.js
@@ -0,0 +1,14 @@
+const mongoose = require(‘mongoose’);
+const userSchema = mongoose.Schema({
+const fileTypes = {
+  {
+  "title": " fetch a post with the ID ",
+  "body": "abc",
+  "comments": [
+    {
+      "name": "john",
+      "email": "john@doe.com",
+      "body": "example body"
+    }
+  ]
+}
--- user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require(‘mongoose’);
-const userSchema = mongoose.Schema({
-   
-     name: { type: String, unique: true,required: true },
-     email: { type: String, required: true },
-     body: { type: String, required: true }, 
-  
-})
